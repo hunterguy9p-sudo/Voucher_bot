1b4509bc83de2ef2932c1420989272ff0c8810b9
hunterguy9p-sudo <hunterguy9p@gmail.com>
2025-11-22 23:04:36 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+# config.py
+
+# ==== BASIC BOT CONFIG ====
+
+# 1) Telegram Bot Token
+BOT_TOKEN = "8343719115:AAGiKM6nzzzg_3Y3qN0aNGazNcgcKtC4HZ4"
+
+# 2) Admin Telegram User ID (tumhara ID)
+ADMIN_ID = 7757877833
+
+# 3) Pay0 user_token / API key
+PAY0_API_KEY = "e5850c2d9bdb7e68e5c8186ac5b6d98f"
+
+# 4) Payment ke baad user jahan redirect hoga
+PAY0_REDIRECT_URL = "https://t.me/coupanestore_bot"  # yahan apna bot username dal sakte ho
+
+# Data JSON file
+DATA_FILE = "data.json"
